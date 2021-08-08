91e0328b4d2978354b93a94715073da05239d90a
sa46lll <9812smh@naver.com>
2021-08-09 00:41:34 +0900

    [Feat] 모델 생성

--- Saengae_server/models.py
+++ Saengae_server/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+# 생리대:성분 = 1:N 관계
+class Pad(models.Model):
+    manufacturer = models.CharField(max_length=10)
+    name = models.CharField(max_length=30)
+    image = models.URLField()
+
+    class Meta:
+        db_table = 'pad'
+
+
+class Ingredient_info(models.Model):
+    pad_name = models.ForeignKey(Pad, on_delete=models.CASCADE)
+    igd_koName = models.CharField(max_length=20)
+    igd_enName = models.CharField(max_length=20)
+    igd_detection = models.FloatField()
+    igd_sideEffect = models.TextField()
+
+    class Meta:
+        db_table = 'ingredient_information'
+
