780dcb19e1c8d099aca08d902c490c46e0ccc0c1
sa46lll <9812smh@naver.com>
2021-08-17 23:56:49 +0900

    [Feat] Ingredient, Detection 모델 추가

--- Saengae_server/models.py
+++ Saengae_server/models.py
@@ -2,11 +2,26 @@ from django.db import models
 
 
 # 생리대:성분 = 1:N 관계
+class Ingredient(models.Model):
+    # pad_name = models.ForeignKey(Pad, on_delete=models.CASCADE)
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    enName = models.CharField(max_length=20)
+    sideEffect = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'ingredient'
+
+
 class Pad(models.Model):
     id = models.BigAutoField(primary_key=True)
-    manufacturer = models.CharField(max_length=20)
     name = models.CharField(max_length=50)
+    manufacturer = models.CharField(max_length=20)
     image = models.CharField(max_length=300)
+    ingredients = models.ManyToManyField(Ingredient, through='Detection')
 
     def __str__(self):
         return self.name
@@ -14,16 +29,15 @@ class Pad(models.Model):
     class Meta:
         db_table = 'pad'
 
-# class Ingredient_info(models.Model):
-#     pad_name = models.ForeignKey(Pad, on_delete=models.CASCADE)
-#     igd_koName = models.CharField(max_length=20)
-#     igd_enName = models.CharField(max_length=20)
-#     igd_detection = models.FloatField()
-#     igd_sideEffect = models.TextField()
-#
-#     def __str__(self):
-#         return self.igd_koName
-#
-#     class Meta:
-#         db_table = 'ingredient_information'
+
+class Detection(models.Model):
+    pad_id = models.ForeignKey(Pad, on_delete=models.CASCADE)
+    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    detection = models.FloatField()
+
+    def __str__(self):
+        return self.detection
+
+    class Meta:
+        db_table = 'detection'
 
